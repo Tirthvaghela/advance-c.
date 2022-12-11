3b5924e7de7ee65c8736da2fc4324a25022a7dcb
Tirth Vaghela <115704585+tirth2830@users.noreply.github.com>
2022-12-11 11:04:30 +0530

    Add files via upload

---
+++ array2,c.c
@@ -0,0 +1,37 @@
+/*Wap to read and print 3-Dimentional matrix in advance c short full code.*/
+
+#include<stdio.h>
+#define N 3
+
+int main()
+{
+    int a[N][N][N];
+    int i, j, k;
+
+    printf("Enter elements in 3-Dimentional matrix of size 3x3x3 : \n");
+    for(i=0; i<N; i++)
+    {
+        for(j=0; j<N; j++)
+        {
+            for(k=0; k<N; k++)
+            {
+                scanf("%d", &a[i][j][k]);
+            }
+        }
+    }
+
+    printf("\nThe matrix is : \n\n");
+    for(i=0; i<N; i++)
+    {
+        for(j=0; j<N; j++)
+        {
+            for(k=0; k<N; k++)
+            {
+                printf("%d ", a[i][j][k]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+}
+
